f69bd583d4e106026f59b2155c49d84925d07528
satosu <narai@utix.co.jp>
2024-10-04 20:51:53 +0900

    詳細ページ

--- pages/[id].vue
+++ pages/[id].vue
@@ -25,12 +25,36 @@
 
 <script setup lang="ts">
 import { Blog } from "~~/types/blog";
+import { useHead } from "#app";
+import { useRoute, useRuntimeConfig } from "#app";
 
 const { params } = useRoute();
+const config = useRuntimeConfig();
 
 const { data } = await useMicroCMSGetListDetail<Blog>({
   endpoint: "blogs",
   contentId: Array.isArray(params.id) ? params.id[0] : params.id,
 });
+
+
+if (data) {
+  // console.log(data.value.title)
+  const currentUrl = `${config.app.baseURL}${params.id}`; // 現在のページのURLを作成
+  console.log(config.app.baseURL)
+  console.log(currentUrl)
+  useHead({
+    title: data.value.title,
+    // meta: [
+    //   {
+    //     property: "og:url",
+    //     content: currentUrl, // og:urlに設定
+    //   },
+    //   {
+    //     property: "og:title",
+    //     content: data.title, // og:titleにも設定
+    //   },
+    // ],
+  });
+}
 // console.log(data)
 </script>
